ea357357308539349e8d762d94caede7a16fe020
jbq2 <jbq2@njit.edu>
2022-04-14 14:03:45 -0400

    better flash messages

--- public_html/Project/helpers.js
+++ public_html/Project/helpers.js
@@ -15,6 +15,45 @@ function flash(message = "", color = "info") {//UCID: jbq2; IT202-010
     flash.appendChild(outerDiv);
 }
 
+function flash (message = "", color = "info") {
+    let flash = document.getElementById("flash");
+    //create a div (or whatever wrapper we want)
+    let outerDiv = document.createElement("div");
+    outerDiv.className = "row justify-content-center";
+    let innerDiv = document.createElement("div");
+
+    //apply the CSS (these are bootstrap classes which we'll learn later)
+    innerDiv.className = `alert alert-${color}`;
+    //set the content
+    innerDiv.innerText = message;
+
+    outerDiv.appendChild(innerDiv);
+    //add the element to the DOM (if we don't it merely exists in memory)
+    flash.appendChild(outerDiv);
+    clear_flashes();
+}
+let flash_timeout = null;
+function clear_flashes () {
+    let flash = document.getElementById("flash");
+    if (!flash_timeout && flash) {
+        flash_timeout = setTimeout(() => {
+            console.log("removing");
+            if (flash.children.length > 0) {
+                flash.children[0].remove();
+            }
+            flash_timeout = null;
+            if (flash.children.length > 0) {
+                clear_flashes();
+            }
+        }, 3000);
+    }
+}
+window.addEventListener("load", () => setTimeout(clear_flashes, 100));
+function isValidUsername (username) {
+    const pattern = /^[a-z0-9_-]{3,16}$/;
+    return pattern.test(username);
+}
+
 function isValidEmail(email){
     if(!/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(email)){
         return false;
