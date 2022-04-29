90dc16610bea53007e400e414837c1d6fa25d20a
jbq2 <jbq2@njit.edu>
2022-04-28 21:34:09 -0400

    purchase history page

---
+++ public_html/Project/purchase_history.php
@@ -0,0 +1,55 @@
+<?php 
+require_once(__DIR__ . "/../../partials/nav.php");
+
+if(!is_logged_in()){
+    flash("You must be logged in to view purchase history", "warning");
+    die(header("Location: login.php"));
+}
+
+$userID = get_user_id();
+$db = getDB();
+
+$statement = $db->prepare("SELECT id, created, total_price, money_received, payment_method
+FROM Orders
+WHERE user_id = :userID");
+$orders = [];
+try{
+    $statement->execute([":userID" => $userID]);
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $orders = $results;
+}
+catch(PDOException $e){
+    flash("Failure in obtaining purchase history", "warning");
+}
+?>
+
+<div>
+    <h1 style="margin-left:0px; margin-bottom:20px">Purchase History</h1>
+    <?php if(count($orders) > 0) : ?>
+        <table style="width:98%">
+            <tr>
+                <td>Order ID (click for more details)</td>
+                <td>Date</td>
+                <td>Total</td>
+                <td>Money Received</td>
+                <td>Payment Method</td>
+            </tr>
+            <?php foreach($orders as $order) : ?>
+                <tr>
+                    <td>
+                        <a style="display:inline-block; margin:0px; text-decoration:none; color:white;" href="order_details.php?id=<?php se($order, "id") ?>">
+                            <div style="display:inline-block; margin:0px;"><?php se($order, "id") ?></div>
+                        </a>
+                    </td>
+                    <td><?php se($order, "created") ?></td>
+                    <td>$<?php se($order, "total_price") ?></td>
+                    <td>$<?php se($order, "money_received") ?></td>
+                    <td><?php se($order, "payment_method") ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php else : ?>
+        <h3><i>No purchases</i></h3>
+    <?php endif; ?>
+</div>
+
